e0585b684c72b6f3cf3ab6a1d199ad5010ed5352
Louie F. Cervantes <lfcervantes@wvsu.edu.ph>
2024-03-20 05:37:35 +0000

    Save

--- pages/1Training_the_Model.py
+++ pages/1Training_the_Model.py
@@ -241,7 +241,7 @@ class CustomCallback(tf.keras.callbacks.Callback):
         
         # Update the Streamlit interface with the current epoch's output
         st.text(f"Epoch {epoch}: loss = {loss:.4f}, accuracy = {accuracy:.4f}")
-        st.session_state.training_history.append({'epoch': None, 'loss': loss, 'accuracy': accuracy})
+        st.session_state.training_history.append({'epoch': epoch, 'loss': loss, 'accuracy': accuracy})
 
 
 #run the app
